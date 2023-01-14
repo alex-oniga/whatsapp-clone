69d1355a14a0f2cb94d489b5eee97b936e00087f
Alex <alex.oniga@mail.com>
2023-01-14 13:57:04 +0200

    add javaScript

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-// function toggleHiddenMenu() {
-//   var x = document.getElementById("hiddenSection");
-//   if (x.style.visibility === "hidden") {
-//     x.style.visibility = "visible";
-//   } else {
-//     x.style.visibility = "hidden";
-//   }
-// }
+function toggleHiddenMenu() {
+  var x = document.getElementById("hiddenSection");
+  if (x.style.visibility === "hidden") {
+    x.style.visibility = "visible";
+  } else {
+    x.style.visibility = "hidden";
+  }
+}
